00e1413cd841b0c2321c59e639d7bc5ccc9a2dd4
Alexander Surma <surma@zib.de>
2011-05-09 11:00:02 +0200

    First working version, threadless though

--- MainWindow.cs
+++ MainWindow.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 using Gtk;
 
 public partial class MainWindow : Gtk.Window
@@ -10,7 +11,7 @@ public partial class MainWindow : Gtk.Window
 	public MainWindow () : base(Gtk.WindowType.Toplevel)
 	{
 		Build ();
-		setupFilelist();
+		SetupFilelist();
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
@@ -18,30 +19,90 @@ public partial class MainWindow : Gtk.Window
 		Application.Quit ();
 		a.RetVal = true;
 	}
-	
-	private void setupFilelist() {
+
+	private void SetupFilelist() {
 		listdata = new Gtk.ListStore(typeof(string));
 		this.filelist.Model = listdata;
 		this.filelist.AppendColumn("Filename", new Gtk.CellRendererText(), "text", 0);
 	}
-	
-	private string getFilename(string fullPath) {
+
+	private string PathToFilename(string fullPath) {
 			string[] segments = new Uri(fullPath).Segments;
 			return segments[segments.Length - 1];
 	}
-	
-	private void loadCurrentFolder() {
+
+	private void LoadCurrentFolder() {
 		this.listdata.Clear();
-		List<string> files = new List<string>(Directory.GetFiles(new Uri(this.folder.Uri).LocalPath));
-		files = files.ConvertAll<string>(new Converter<string, string>(this.getFilename));
+		List<string> files = new List<string>(Directory.GetFiles(new Uri(this.directorychooser.Uri).LocalPath));
+		files = files.ConvertAll<string>(new Converter<string, string>(this.PathToFilename));
 		foreach(string file in files) {
 			this.listdata.AppendValues(file);
 		}
 	}
+
+	protected virtual void LoadFolderEvent (object sender, System.EventArgs e) {
+		this.LoadCurrentFolder();
+	}
+
+	private void RenameFilesThreadStart(object param) {
+		RenameFiles();
+	}
+
+	private void Log(String line) {
+		this.logview.Buffer.Text += line+"\n";
+		Gtk.ScrolledWindow view = (Gtk.ScrolledWindow)this.logview.Parent;
+		view.Vadjustment.Value = view.Vadjustment.Upper;
+	}
+
+	private void RenameFiles() {
+		string template = this.templatefield.Text;
+		int i = 0;
+		foreach(object[] rowitem in this.listdata) {
+			i++;
+			string filename = (string)rowitem[0];
+			string filepath = new Uri(this.directorychooser.Uri + "/" + filename).LocalPath;
+			string extension = System.IO.Path.GetExtension(filename);
+			string newfilename = String.Format(template, i) + extension;
+			string newfilepath = new Uri(this.directorychooser.Uri + "/" + newfilename).LocalPath;
+
+			Log(filename + " => " + newfilename);
+			try {
+				File.Move(filepath, newfilepath);
+			} catch (IOException e) {
+				Log("\t"+e.Message);
+				continue;
+			}
+		}
+		LoadCurrentFolder();
+		ProcessDone();
+	}
+
+	private void ProcessStarted() {
+		this.directorychooser.Sensitive = false;
+		this.templatefield.Sensitive = false;
+		this.applybutton.Sensitive = false;
+		this.refreshbutton.Sensitive = false;
+		this.cancelbutton.Sensitive = true;
+	}
+
+	private void ProcessDone() {
+		this.directorychooser.Sensitive = true;
+		this.templatefield.Sensitive = true;
+		this.applybutton.Sensitive = true;
+		this.refreshbutton.Sensitive = true;
+		this.cancelbutton.Sensitive = false;
+	}
+
+	protected virtual void RenameFilesEvent (object sender, System.EventArgs e)
+	{
+		ProcessStarted();
+		//Thread t = new Thread(new ParameterizedThreadStart(this.RenameFilesThreadStart));
+		//t.Start();
+		this.RenameFiles();
+	}
 	
-	protected virtual void loadFolder (object sender, System.EventArgs e) {
-		this.loadCurrentFolder();
+	protected virtual void ShowHelpEvent (object sender, System.EventArgs e)
+	{
+		System.Diagnostics.Process.Start("http://blog.stevex.net/string-formatting-in-csharp/");
 	}
 }
-	
-	
--- gtk-gui/MainWindow.cs
+++ gtk-gui/MainWindow.cs
@@ -7,13 +7,17 @@ public partial class MainWindow
 
 	private global::Gtk.Table table1;
 
-	private global::Gtk.FileChooserButton folder;
+	private global::Gtk.Button button45;
+
+	private global::Gtk.FileChooserButton directorychooser;
 
 	private global::Gtk.Label label1;
 
 	private global::Gtk.Label label2;
 
-	private global::Gtk.Entry template;
+	private global::Gtk.Button refreshbutton;
+
+	private global::Gtk.Entry templatefield;
 
 	private global::Gtk.HBox hbox1;
 
@@ -21,13 +25,17 @@ public partial class MainWindow
 
 	private global::Gtk.TreeView filelist;
 
-	private global::Gtk.VButtonBox vbuttonbox1;
+	private global::Gtk.HButtonBox hbuttonbox1;
 
-	private global::Gtk.Button @remove;
+	private global::Gtk.Button applybutton;
 
-	private global::Gtk.Button reload;
+	private global::Gtk.Button cancelbutton;
 
-	private global::Gtk.Button save;
+	private global::Gtk.HSeparator hseparator1;
+
+	private global::Gtk.ScrolledWindow GtkScrolledWindow1;
+
+	private global::Gtk.TextView logview;
 
 	protected virtual void Build ()
 	{
@@ -36,59 +44,90 @@ public partial class MainWindow
 		this.Name = "MainWindow";
 		this.Title = global::Mono.Unix.Catalog.GetString ("Enumerator");
 		this.WindowPosition = ((global::Gtk.WindowPosition)(4));
+		this.BorderWidth = ((uint)(6));
 		// Container child MainWindow.Gtk.Container+ContainerChild
 		this.vbox1 = new global::Gtk.VBox ();
 		this.vbox1.Name = "vbox1";
 		this.vbox1.Spacing = 6;
 		// Container child vbox1.Gtk.Box+BoxChild
-		this.table1 = new global::Gtk.Table (((uint)(2)), ((uint)(2)), false);
+		this.table1 = new global::Gtk.Table (((uint)(2)), ((uint)(3)), false);
 		this.table1.Name = "table1";
 		this.table1.RowSpacing = ((uint)(6));
 		this.table1.ColumnSpacing = ((uint)(6));
 		// Container child table1.Gtk.Table+TableChild
-		this.folder = new global::Gtk.FileChooserButton (global::Mono.Unix.Catalog.GetString ("Select Folder"), ((global::Gtk.FileChooserAction)(2)));
-		this.folder.Name = "folder";
-		this.table1.Add (this.folder);
-		global::Gtk.Table.TableChild w1 = ((global::Gtk.Table.TableChild)(this.table1[this.folder]));
-		w1.LeftAttach = ((uint)(1));
-		w1.RightAttach = ((uint)(2));
+		this.button45 = new global::Gtk.Button ();
+		this.button45.CanFocus = true;
+		this.button45.Name = "button45";
+		this.button45.UseStock = true;
+		this.button45.UseUnderline = true;
+		this.button45.Label = "gtk-help";
+		this.table1.Add (this.button45);
+		global::Gtk.Table.TableChild w1 = ((global::Gtk.Table.TableChild)(this.table1[this.button45]));
+		w1.TopAttach = ((uint)(1));
+		w1.BottomAttach = ((uint)(2));
+		w1.LeftAttach = ((uint)(2));
+		w1.RightAttach = ((uint)(3));
+		w1.XOptions = ((global::Gtk.AttachOptions)(4));
 		w1.YOptions = ((global::Gtk.AttachOptions)(4));
+		// Container child table1.Gtk.Table+TableChild
+		this.directorychooser = new global::Gtk.FileChooserButton (global::Mono.Unix.Catalog.GetString ("Select Folder"), ((global::Gtk.FileChooserAction)(2)));
+		this.directorychooser.Name = "directorychooser";
+		this.table1.Add (this.directorychooser);
+		global::Gtk.Table.TableChild w2 = ((global::Gtk.Table.TableChild)(this.table1[this.directorychooser]));
+		w2.LeftAttach = ((uint)(1));
+		w2.RightAttach = ((uint)(2));
+		w2.YOptions = ((global::Gtk.AttachOptions)(4));
 		// Container child table1.Gtk.Table+TableChild
 		this.label1 = new global::Gtk.Label ();
 		this.label1.Name = "label1";
-		this.label1.LabelProp = global::Mono.Unix.Catalog.GetString ("Folder:");
+		this.label1.Xalign = 1f;
+		this.label1.LabelProp = global::Mono.Unix.Catalog.GetString ("Directory:");
 		this.table1.Add (this.label1);
-		global::Gtk.Table.TableChild w2 = ((global::Gtk.Table.TableChild)(this.table1[this.label1]));
-		w2.XOptions = ((global::Gtk.AttachOptions)(4));
-		w2.YOptions = ((global::Gtk.AttachOptions)(4));
+		global::Gtk.Table.TableChild w3 = ((global::Gtk.Table.TableChild)(this.table1[this.label1]));
+		w3.XOptions = ((global::Gtk.AttachOptions)(4));
+		w3.YOptions = ((global::Gtk.AttachOptions)(4));
 		// Container child table1.Gtk.Table+TableChild
 		this.label2 = new global::Gtk.Label ();
 		this.label2.Name = "label2";
 		this.label2.LabelProp = global::Mono.Unix.Catalog.GetString ("Template Name:");
 		this.table1.Add (this.label2);
-		global::Gtk.Table.TableChild w3 = ((global::Gtk.Table.TableChild)(this.table1[this.label2]));
-		w3.TopAttach = ((uint)(1));
-		w3.BottomAttach = ((uint)(2));
-		w3.XOptions = ((global::Gtk.AttachOptions)(4));
-		w3.YOptions = ((global::Gtk.AttachOptions)(4));
-		// Container child table1.Gtk.Table+TableChild
-		this.template = new global::Gtk.Entry ();
-		this.template.CanFocus = true;
-		this.template.Name = "template";
-		this.template.IsEditable = true;
-		this.template.InvisibleChar = '●';
-		this.table1.Add (this.template);
-		global::Gtk.Table.TableChild w4 = ((global::Gtk.Table.TableChild)(this.table1[this.template]));
+		global::Gtk.Table.TableChild w4 = ((global::Gtk.Table.TableChild)(this.table1[this.label2]));
 		w4.TopAttach = ((uint)(1));
 		w4.BottomAttach = ((uint)(2));
-		w4.LeftAttach = ((uint)(1));
-		w4.RightAttach = ((uint)(2));
+		w4.XOptions = ((global::Gtk.AttachOptions)(4));
 		w4.YOptions = ((global::Gtk.AttachOptions)(4));
+		// Container child table1.Gtk.Table+TableChild
+		this.refreshbutton = new global::Gtk.Button ();
+		this.refreshbutton.CanFocus = true;
+		this.refreshbutton.Name = "refreshbutton";
+		this.refreshbutton.UseStock = true;
+		this.refreshbutton.UseUnderline = true;
+		this.refreshbutton.Label = "gtk-refresh";
+		this.table1.Add (this.refreshbutton);
+		global::Gtk.Table.TableChild w5 = ((global::Gtk.Table.TableChild)(this.table1[this.refreshbutton]));
+		w5.LeftAttach = ((uint)(2));
+		w5.RightAttach = ((uint)(3));
+		w5.XOptions = ((global::Gtk.AttachOptions)(4));
+		w5.YOptions = ((global::Gtk.AttachOptions)(4));
+		// Container child table1.Gtk.Table+TableChild
+		this.templatefield = new global::Gtk.Entry ();
+		this.templatefield.CanFocus = true;
+		this.templatefield.Name = "templatefield";
+		this.templatefield.Text = global::Mono.Unix.Catalog.GetString ("Tv.Show.s01e{0:00}");
+		this.templatefield.IsEditable = true;
+		this.templatefield.InvisibleChar = '●';
+		this.table1.Add (this.templatefield);
+		global::Gtk.Table.TableChild w6 = ((global::Gtk.Table.TableChild)(this.table1[this.templatefield]));
+		w6.TopAttach = ((uint)(1));
+		w6.BottomAttach = ((uint)(2));
+		w6.LeftAttach = ((uint)(1));
+		w6.RightAttach = ((uint)(2));
+		w6.YOptions = ((global::Gtk.AttachOptions)(4));
 		this.vbox1.Add (this.table1);
-		global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.table1]));
-		w5.Position = 0;
-		w5.Expand = false;
-		w5.Fill = false;
+		global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.table1]));
+		w7.Position = 0;
+		w7.Expand = false;
+		w7.Fill = false;
 		// Container child vbox1.Gtk.Box+BoxChild
 		this.hbox1 = new global::Gtk.HBox ();
 		this.hbox1.Name = "hbox1";
@@ -104,54 +143,66 @@ public partial class MainWindow
 		this.filelist.Reorderable = true;
 		this.GtkScrolledWindow.Add (this.filelist);
 		this.hbox1.Add (this.GtkScrolledWindow);
-		global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(this.hbox1[this.GtkScrolledWindow]));
-		w7.Position = 0;
-		// Container child hbox1.Gtk.Box+BoxChild
-		this.vbuttonbox1 = new global::Gtk.VButtonBox ();
-		this.vbuttonbox1.Name = "vbuttonbox1";
-		this.vbuttonbox1.LayoutStyle = ((global::Gtk.ButtonBoxStyle)(3));
-		// Container child vbuttonbox1.Gtk.ButtonBox+ButtonBoxChild
-		this.@remove = new global::Gtk.Button ();
-		this.@remove.CanFocus = true;
-		this.@remove.Name = "remove";
-		this.@remove.UseStock = true;
-		this.@remove.UseUnderline = true;
-		this.@remove.Label = "gtk-stop";
-		this.vbuttonbox1.Add (this.@remove);
-		global::Gtk.ButtonBox.ButtonBoxChild w8 = ((global::Gtk.ButtonBox.ButtonBoxChild)(this.vbuttonbox1[this.@remove]));
-		w8.Expand = false;
-		w8.Fill = false;
-		// Container child vbuttonbox1.Gtk.ButtonBox+ButtonBoxChild
-		this.reload = new global::Gtk.Button ();
-		this.reload.CanFocus = true;
-		this.reload.Name = "reload";
-		this.reload.UseStock = true;
-		this.reload.UseUnderline = true;
-		this.reload.Label = "gtk-refresh";
-		this.vbuttonbox1.Add (this.reload);
-		global::Gtk.ButtonBox.ButtonBoxChild w9 = ((global::Gtk.ButtonBox.ButtonBoxChild)(this.vbuttonbox1[this.reload]));
-		w9.Position = 1;
-		w9.Expand = false;
-		w9.Fill = false;
-		this.hbox1.Add (this.vbuttonbox1);
-		global::Gtk.Box.BoxChild w10 = ((global::Gtk.Box.BoxChild)(this.hbox1[this.vbuttonbox1]));
-		w10.Position = 1;
-		w10.Expand = false;
-		w10.Fill = false;
+		global::Gtk.Box.BoxChild w9 = ((global::Gtk.Box.BoxChild)(this.hbox1[this.GtkScrolledWindow]));
+		w9.Position = 0;
 		this.vbox1.Add (this.hbox1);
-		global::Gtk.Box.BoxChild w11 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.hbox1]));
-		w11.Position = 1;
+		global::Gtk.Box.BoxChild w10 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.hbox1]));
+		w10.Position = 1;
 		// Container child vbox1.Gtk.Box+BoxChild
-		this.save = new global::Gtk.Button ();
-		this.save.CanFocus = true;
-		this.save.Name = "save";
-		this.save.UseUnderline = true;
-		this.save.Label = global::Mono.Unix.Catalog.GetString ("Rename All");
-		this.vbox1.Add (this.save);
-		global::Gtk.Box.BoxChild w12 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.save]));
-		w12.Position = 2;
+		this.hbuttonbox1 = new global::Gtk.HButtonBox ();
+		this.hbuttonbox1.Name = "hbuttonbox1";
+		// Container child hbuttonbox1.Gtk.ButtonBox+ButtonBoxChild
+		this.applybutton = new global::Gtk.Button ();
+		this.applybutton.CanFocus = true;
+		this.applybutton.Name = "applybutton";
+		this.applybutton.UseStock = true;
+		this.applybutton.UseUnderline = true;
+		this.applybutton.Label = "gtk-apply";
+		this.hbuttonbox1.Add (this.applybutton);
+		global::Gtk.ButtonBox.ButtonBoxChild w11 = ((global::Gtk.ButtonBox.ButtonBoxChild)(this.hbuttonbox1[this.applybutton]));
+		w11.Expand = false;
+		w11.Fill = false;
+		// Container child hbuttonbox1.Gtk.ButtonBox+ButtonBoxChild
+		this.cancelbutton = new global::Gtk.Button ();
+		this.cancelbutton.Sensitive = false;
+		this.cancelbutton.CanFocus = true;
+		this.cancelbutton.Name = "cancelbutton";
+		this.cancelbutton.UseStock = true;
+		this.cancelbutton.UseUnderline = true;
+		this.cancelbutton.Label = "gtk-cancel";
+		this.hbuttonbox1.Add (this.cancelbutton);
+		global::Gtk.ButtonBox.ButtonBoxChild w12 = ((global::Gtk.ButtonBox.ButtonBoxChild)(this.hbuttonbox1[this.cancelbutton]));
+		w12.Position = 1;
 		w12.Expand = false;
 		w12.Fill = false;
+		this.vbox1.Add (this.hbuttonbox1);
+		global::Gtk.Box.BoxChild w13 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.hbuttonbox1]));
+		w13.Position = 2;
+		w13.Expand = false;
+		w13.Fill = false;
+		// Container child vbox1.Gtk.Box+BoxChild
+		this.hseparator1 = new global::Gtk.HSeparator ();
+		this.hseparator1.Name = "hseparator1";
+		this.vbox1.Add (this.hseparator1);
+		global::Gtk.Box.BoxChild w14 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.hseparator1]));
+		w14.Position = 3;
+		w14.Expand = false;
+		w14.Fill = false;
+		// Container child vbox1.Gtk.Box+BoxChild
+		this.GtkScrolledWindow1 = new global::Gtk.ScrolledWindow ();
+		this.GtkScrolledWindow1.Name = "GtkScrolledWindow1";
+		this.GtkScrolledWindow1.ShadowType = ((global::Gtk.ShadowType)(1));
+		// Container child GtkScrolledWindow1.Gtk.Container+ContainerChild
+		this.logview = new global::Gtk.TextView ();
+		this.logview.CanFocus = true;
+		this.logview.Name = "logview";
+		this.logview.Editable = false;
+		this.logview.CursorVisible = false;
+		this.GtkScrolledWindow1.Add (this.logview);
+		this.vbox1.Add (this.GtkScrolledWindow1);
+		global::Gtk.Box.BoxChild w16 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.GtkScrolledWindow1]));
+		w16.Position = 4;
+		w16.Expand = false;
 		this.Add (this.vbox1);
 		if ((this.Child != null)) {
 			this.Child.ShowAll ();
@@ -160,6 +211,8 @@ public partial class MainWindow
 		this.DefaultHeight = 300;
 		this.Show ();
 		this.DeleteEvent += new global::Gtk.DeleteEventHandler (this.OnDeleteEvent);
-		this.folder.SelectionChanged += new global::System.EventHandler (this.loadFolder);
+		this.refreshbutton.Clicked += new global::System.EventHandler (this.LoadFolderEvent);
+		this.directorychooser.SelectionChanged += new global::System.EventHandler (this.LoadFolderEvent);
+		this.applybutton.Clicked += new global::System.EventHandler (this.RenameFilesEvent);
 	}
 }
